b11b85b1fdf2d992a6bc769ee0c61caf31a86a8b
Vivek chaurasia <vivekkumarr221106@gmail.com>
2026-03-20 22:33:21 +0530

    Exploring response

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Data struct {
+	Elevation float64 `json:"elevation"`
+	Hourly map[string]any `json:"hourly"`
+}
 
 func main(){
 	endpoint := "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"    //A string variable storing API URL
@@ -12,5 +18,14 @@ func main(){
 	if err != nil{
 		log.Fatal(err)
 	}
-	fmt.Println(resp)
+	// Lets Explore The Response...
+
+	// var data map[string]map[string]any
+
+	var data Data
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil{
+		log.Fatal(err)
+	}
+	// fmt.Println(data["hourly"]["temperature_2m"])
+	fmt.Println(data)
 }
